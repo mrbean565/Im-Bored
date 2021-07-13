0c84165aed54d3bed82a570b3aeef8bf9a39da3d
mr bean <67719111+mrbean565@users.noreply.github.com>
2021-07-13 04:52:28 +0100

    Create pat.py

---
+++ pat/pat.py
@@ -0,0 +1,13 @@
+import discord 
+import json
+from discord.ext.commands import CommandNotFound
+from tabulate import tabulate 
+from discord.ext import commands
+from discord import Game
+from discord.ext.commands import Bot
+from pip._vendor import requests
+
+
+
+
+token = ''
